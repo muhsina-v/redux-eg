10421954f847bf30c3d08e34bdcedc482363f512
muhsina <muhsinamolty44@gmail.com>
2025-02-23 22:35:37 +0530

    started todo app

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 
 function Todo() {
-    const dispatch=useDispatch();
-    
+    const Dispatch=useDispatch();
+    const tasks=useSelector(stete=>state.todo.tasks)
+    const {task,settask}=useState("");
+    const {is}
 
   return (
     <div>Todo</div>
